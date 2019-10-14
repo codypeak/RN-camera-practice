4481e37f6e271c840825d90455d2f49cbb479c3c
Ian Cameron <icnyny@gmail.com>
2019-10-13 21:19:06 -0600

    adds sqlite

--- store/places-reducer.js
+++ store/places-reducer.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE } from './places-actions';
+import { ADD_PLACE, SET_PLACES } from './places-actions';
 import Place from '../models/place';
 
 
@@ -8,9 +8,17 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case SET_PLACES: 
+            return {
+                places: action.placeData.map(pl => new Place(
+                    pl.id.toString(),
+                    pl.title,
+                    pl.imageUri,
+                ))
+            }
         case ADD_PLACE: 
             const newPlace = new Place(
-                new Date().toString(),
+                action.placeData.id.toString(),
                 action.placeData.title,
                 action.placeData.image,
             );
